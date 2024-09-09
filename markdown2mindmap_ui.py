9de14fdab45db99373d155d5cb3d8b65b0dd11fa
Lukasz Przewlocki <lukasz.przewlocki2@sky.uk>
2024-09-10 00:08:05 +0100

    auto-reformatting

--- markdown2mindmap_ui.py
+++ markdown2mindmap_ui.py
@@ -2,9 +2,6 @@ import tkinter as tk
 from tkinter import scrolledtext
 import threading
 import queue
-import re
-import time
-from functools import partial
 
 from markdown_to_indent.markdown_to_indent import markdown_to_indent
 
@@ -15,10 +12,6 @@ OUTPUT_WIDTH = 80
 OUTPUT_HEIGHT = 15
 BUTTON_WIDTH = 20
 
-# # Function to convert Markdown to Indent
-# def markdown_to_indent(text):
-#     # Implement your Markdown to Indent conversion logic here
-#     return text  # For demonstration purposes, just return the input
 
 # Function to handle button click
 def convert_and_display():
@@ -29,6 +22,7 @@ def convert_and_display():
     output_field.insert(tk.END, output_text)
     output_field.config(state="disabled")
 
+
 # Create the main window
 root = tk.Tk()
 root.title("Markdown to Indent Converter")
@@ -36,22 +30,30 @@ root.title("Markdown to Indent Converter")
 # Create the input field
 input_label = tk.Label(root, text="Input:", font=("Arial", 12))
 input_label.grid(row=0, column=0)
-input_field = scrolledtext.ScrolledText(root, width=INPUT_WIDTH, height=INPUT_HEIGHT, wrap=tk.WORD)
+input_field = scrolledtext.ScrolledText(
+    root, width=INPUT_WIDTH, height=INPUT_HEIGHT, wrap=tk.WORD
+)
 input_field.grid(row=1, column=0, columnspan=2)
 
 # Create the output field
 output_label = tk.Label(root, text="Output:", font=("Arial", 12))
 output_label.grid(row=2, column=0)
-output_field = scrolledtext.ScrolledText(root, width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT, wrap=tk.WORD)
+output_field = scrolledtext.ScrolledText(
+    root, width=OUTPUT_WIDTH, height=OUTPUT_HEIGHT, wrap=tk.WORD
+)
 output_field.grid(row=3, column=0, columnspan=2)
 output_field.config(state="disabled")
 
 # Create the conversion button
-button = tk.Button(root, text="Convert", width=BUTTON_WIDTH, command=convert_and_display)
+button = tk.Button(
+    root, text="Convert", width=BUTTON_WIDTH, command=convert_and_display
+)
 button.grid(row=4, column=0, columnspan=2)
 
 # Implement the delay functionality
 input_queue = queue.Queue()
+
+
 def check_input_and_convert():
     global last_input
     current_input = input_field.get(1.0, tk.END)
@@ -59,6 +61,7 @@ def check_input_and_convert():
         threading.Thread(target=convert_and_display).start()
         last_input = current_input
     root.after(1000, check_input_and_convert)
+
 
 last_input = input_field.get(1.0, tk.END)
 root.after(1000, check_input_and_convert)
