c00e4a90ecc72f49c73382fa272f1c20c25ef590
Lukasz Przewlocki <lukasz.przewlocki2@sky.uk>
2024-09-09 23:02:16 +0100

    latest updates

--- markdown2mindmap.py
+++ markdown2mindmap.py
@@ -1,6 +1,5 @@
 import re
 
-
 input_file = '_input_markdown.md'
 output_file = '_output_indent.txt'
 
@@ -11,11 +10,8 @@ def convert_markdown_to_indent(input_file, output_file):
             return len(header_match.group(1)) - 1
         list_match = re.match(r'^(\s*[\-\+])\s', line)
         if list_match:
-            indent_len = int(len(list_match.group(1)) / 2.0)
-            # print(f"\nline: {line}")
-            # print(f"indent_len: {indent_len}")
-            return indent_len
-        return 0
+            return len(list_match.group(1)) // 2
+        return -1  # Return -1 for plain lines
 
     def convert_bold_markers(text):
         return re.sub(r'^(.*?)\*\*(.*?)\*\*(.*)$', r'<html>\1<b>\2</b>\3</html>', text)
@@ -24,22 +20,36 @@ def convert_markdown_to_indent(input_file, output_file):
         return re.match(r'^\[.*\]$', line.strip()) is not None
 
     with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', encoding='utf-8') as outfile:
-        next_indent_level = 0
+        current_indent = 0
+        last_header_indent = 0
+        last_plain_indent = 0
+
         for line in infile:
             if is_reset_marker(line):
-                # Write the reset marker with no indentation
                 outfile.write(line.strip() + '\n')
-                next_indent_level = 1  # Reset the next indentation level to 1
+                current_indent = 0
+                last_header_indent = 0
+                last_plain_indent = 0
                 continue
 
-            indent_level = get_indent_level(line) + next_indent_level
+            line_indent = get_indent_level(line)
+
+            if line_indent == -1:  # Plain line
+                current_indent = last_header_indent + 1
+                last_plain_indent = current_indent
+            elif re.match(r'^#+\s', line):  # Header
+                current_indent = line_indent
+                last_header_indent = current_indent
+            elif re.match(r'^\s*[-+]\s', line):  # List item
+                current_indent = last_plain_indent + 1
+
             if re.match(r'^\s*[-+]\s', line):
                 line_content = line.lstrip(' -+').strip()
             else:
                 line_content = line.lstrip('#').strip()
 
             formatted_line = convert_bold_markers(line_content)
-            indented_line = '  ' * indent_level + formatted_line + '\n'
+            indented_line = '  ' * current_indent + formatted_line + '\n'
             outfile.write(indented_line)
 
 if __name__ == "__main__":
