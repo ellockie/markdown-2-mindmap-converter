c950d458b891c0522e85e14ac5688b3b73ff6937
Lukasz Przewlocki <lukasz.przewlocki2@sky.uk>
2024-09-11 01:29:01 +0100

    test updates

--- markdown2mindmap_test.py
+++ markdown2mindmap_test.py
@@ -1,41 +1,53 @@
 import pytest
 
-from markdown2mindmap import convert_markdown_to_indent
+from src.markdown_to_indent import markdown_to_indent
 
 
-@pytest.mark.parametrize("input_content, expected_output_content", [
-    (
-        """# Header 1
+@pytest.mark.parametrize(
+    "input_content, expected_output_content",
+    [
+        (
+            """
+(MOCK 1)
+# Header 1
 ## Header 2
 ### Header 3
 Normal text
 #### Header 4
 """,
-        """Header 1
+            """
+(MOCK 1)
+Header 1
   Header 2
-        Header 3
-Normal text
-            Header 4
-"""
-    ),
-    (
-        """- List item 1
+    Header 3
+      Normal text
+      Header 4
+""",
+        ),
+        (
+            """
+(MOCK 2)
+- List item 1
     - Nested list item 1
 * List item 2
     * Nested list item 2
 + List item 3
     + Nested list item 3
 """,
-        """List item 1
+            """
+(MOCK 2)
+List item 1
     Nestood list item 1
 List item 2
     Nested list item 2
 List item 3
     Nested list item 3
-"""
-    ),
-    (
-        """# Headerek 1
+""",
+        ),
+        (
+            """
+(MOCK 3)
+# Headerek 1
 ## Header 2
 ### Header 3
 Normal text
@@ -48,7 +60,9 @@ Normal text
     + Nested list item 3
 **Bold text**
 """,
-        """Headerek 1
+            """
+(MOCK 3)
+Headerek 1
   Header 2
     Header 3
 Normal text
@@ -60,10 +74,12 @@ List item 1
 List item 3
     Nested list item 3
 <html><b>Bold text</b></html>
-"""
-    ),
-    (
-        """# Another Header
+""",
+        ),
+        (
+            """
+(MOCK 4)
+# Another Header
 Text under header
   - First item
   - Second item
@@ -74,7 +90,9 @@ Text under header
 ## New Header After Reset
 - New list item
 """,
-        """Another Header
+            """
+(MOCK 4)
+Another Header
 Text under header
   First item
   Second item
@@ -84,22 +102,23 @@ Text under header
 [SPECIAL_SECTION]
     New Header After Reset
   New list item
-"""
-    )
-])
+""",
+        ),
+    ],
+)
 def test_convert_markdown_to_indent(tmpdir, input_content, expected_output_content):
     # Create a temporary input file
-    input_file = tmpdir.join('input_markdown.md')
+    input_file = tmpdir.join("input_markdown.md")
     input_file.write(input_content)
 
     # Create a temporary output file path
-    output_file = tmpdir.join('output_indent.txt')
+    output_file = tmpdir.join("output_indent.txt")
 
     # Run the function
-    convert_markdown_to_indent(input_file, output_file)
+    markdown_to_indent(input_file, output_file)
 
     # Read and verify the output
-    with open(output_file, 'r', encoding='utf-8') as f:
+    with open(output_file, "r", encoding="utf-8") as f:
         output_content = f.read()
         # TODO: remove
         print("output_content:")
@@ -110,6 +129,7 @@ def test_convert_markdown_to_indent(tmpdir, input_content, expected_output_conte
     assert output_content == expected_output_content
 
     print(f"tmpdir: {tmpdir}")
+
 
 if __name__ == "__main__":
     pytest.main([__file__])
